cba72afa6134277f0ce66ad5187097cf84a6b89e
PetroTelishevskyi <34678446+PetroTelishevskyi@users.noreply.github.com>
2020-11-09 21:42:20 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu, .intro__info__inner').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+function selectTab(num) {
+    for (var i=1; i <= 7; i++) {
+      document.getElementById("tab" + i).className = "";
+      document.getElementById("box" + i).className = "infobox";
+    }
+    document.getElementById("tab" + num).className = "selected";
+    document.getElementById("box" + num).className = "infobox enabled";
+};
